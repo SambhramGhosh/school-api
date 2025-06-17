de5e367fa998aee5c342ef65920459c443c78791
Sambhram Ghosh <sambhramghosh@gmail.com>
2025-06-17 20:54:48 +0530

    Added start script

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const schoolRoutes = require('./routes/schoolRoutes');
 app.use(express.json());
 app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
