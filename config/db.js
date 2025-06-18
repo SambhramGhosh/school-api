157c06f2fb0ae437d6ce2ec2918f754f2e8fe01a
Sambhram Ghosh <sambhramghosh@gmail.com>
2025-06-18 14:00:16 +0530

    testing v3

--- config/db.js
+++ config/db.js
@@ -3,14 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  queueLimit: 0
+  port: process.env.DB_PORT || 3306
 });
 
 pool.getConnection((err, connection) => {
